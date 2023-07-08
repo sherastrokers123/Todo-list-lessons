42ed0133ddcc39c16ceb5f439c53c665d46e3f41
Cessabit <sherastrokers123@yandex.ru>
2023-07-09 00:52:02 +0300

    udd

--- Todo-2/src/App.tsx
+++ Todo-2/src/App.tsx
@@ -2,10 +2,9 @@ import React, {useState} from 'react';
 import './App.css';
 import {TasksType, TodoList} from "./Components/TodoList";
 
-export type TasksValuesType = "All" | "Active" | "Completed";
+export type ValueTaskType = "All" | "Active" | "Completed";
 
 function App() {
-
     let [tasks, setTasks] = useState<Array<TasksType>>(
         [
             {id: 1, title: "HTML&CSS", isDone: true},
@@ -17,32 +16,24 @@ function App() {
     const removeTask = (taskID: number) => {
         setTasks(tasks.filter(task => task.id !== taskID));
     }
-
+    // filter
+    const [filter,setFilter]=useState("All");
+    let filteredTask = tasks;
+    if(filter === "Completed"){
+        filteredTask = tasks.filter(task=>task.isDone===true)
+    }
+    if(filter === "Active"){
+        filteredTask = tasks.filter(task=>task.isDone===true)
+    }
     // filter
 
-    let filteredTask = tasks
-    let filteringOfTasks = (value: TasksValuesType) => {
-        if (value === 'Active') {
-            let filteredTask = tasks.filter(task => !task.isDone)
-            console.log(filteredTask)
-        }
-        if (value === 'Completed') {
-            let filteredTask = tasks.filter(task => task.isDone)
-            console.log(filteredTask)
-        }
-    }
-
-
-    // -filter//
-
-    // передача функций
+    // передача функций в Todolist
     return (
         <div className='App'>
             <TodoList
                 title='What to learn'
                 tasks={filteredTask}
                 removeTask={removeTask}
-                filteringOfTasks={filteringOfTasks}
             />
         </div>
     );
--- Todo-2/src/Components/TodoList.tsx
+++ Todo-2/src/Components/TodoList.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import {TasksValuesType} from "../App";
 // типизация компоненты
 type TodoListPropsType = {
     title: string,
     tasks: TasksType[],
     removeTask: (taskID: number) => void,
-    filteringOfTasks:(value:TasksValuesType)=>void,
 }
 // типизация таски
 export type TasksType = {
@@ -33,9 +31,9 @@ export const TodoList = (props: TodoListPropsType) => {
                 })}
             </ul>
             <div>
-                <button onClick={()=>props.filteringOfTasks("All")}>All</button>
-                <button onClick={()=>props.filteringOfTasks('Active')}>Active</button>
-                <button onClick={()=>props.filteringOfTasks('Completed')}>Completed</button>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
             </div>
         </div>
     )
